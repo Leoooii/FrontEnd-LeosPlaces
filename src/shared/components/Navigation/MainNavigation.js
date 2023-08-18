ca6ec609f9185ebbe492f6b4b6c0b839f2d7eed8
Leoooii <lioneh39@gmail.com>
2023-08-18 16:00:17 +0300

    first commit

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
+
+const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerisOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerisOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerisOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Leo'sPlaces+</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
